e43ed833cdf5ae6ca9add240278f1a030a8146dc
yuekaim <yuekaim@gmail.com>
2021-09-10 19:03:21 +0200

    panel install

--- site/config/config.php
+++ site/config/config.php
@@ -2,9 +2,7 @@
 
 return [
   'debug'  => true,
-  'panel' =>[
-      'install' => true
-    ],
+
   'home' => 'home',
   'komment-auto-publish' => true,
   'url' => 'https://mkg-survey.suborder.center/'
