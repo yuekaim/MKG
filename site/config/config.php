50cd76bfec0b99225086c6d27be1c58998a55c4d
yuekaim <yuekaim@gmail.com>
2021-09-10 19:04:05 +0200

    panel slug

--- site/config/config.php
+++ site/config/config.php
@@ -2,7 +2,10 @@
 
 return [
   'debug'  => true,
-
+  'panel' =>[
+      'install' => true,
+      'slug' => 'panel'
+    ],
   'home' => 'home',
   'komment-auto-publish' => true,
   'url' => 'https://mkg-survey.suborder.center/'
